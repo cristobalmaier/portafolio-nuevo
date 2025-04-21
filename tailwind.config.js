718319a45c49fc64ae1aeaf0914a6c938e5efa0c
chinogenio8 <cristobalmaier1@gmail.com>
2025-04-21 16:07:42 -0300

    mejoras en la iu, falta pero vamos por buen camino

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
     content: ["./*.html"],
     theme: {
       extend: {
+        fontFamily: {
+          montserrat: ['Montserrat', 'sans-serif'],
+        },
         animation: {
           'fade-in': 'fadeIn 1s ease-in-out',
         },
